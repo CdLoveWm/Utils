a2ba21d9c1d04a06ab7955730c58abb852b49d58
chendong <1039270526@qq.com>
2022-11-28 16:57:09 +0800

    添加类

---
+++ Utils.Docment.Convert/ExcelAndPdf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Docment.Convert
+{
+    /// <summary>
+    /// EXCEL 和 PDF 操作类
+    /// </summary>
+    public class ExcelAndPdf
+    {
+        /// <summary>
+        /// Excel转换为Pdf
+        /// </summary>
+        /// <param name="fileSrc"></param>
+        public void ConvertExcelToPdf(string fileSrc)
+        {
+
+        }
+    }
+}
---
+++ Utils.Docment.Convert/readme.md
@@ -0,0 +1,7 @@
+﻿本项目主要采用的是Spire的免费版本
+
+#### FreeSpire.XLS
+
+FreeSpire.XLS是Spire的社区版，是免费的Excel API。可以用于个人或者商业用途。但是免费版在功能上有一定的限制：
+
+> 免费版限制为每个工作簿 5 张，每张 200 行。在读取或写入 XLS 或 PDF 文件期间强制实施此限制。从Free Spire.XLS v7.8开始，加载和保存.xlsx文件格式时没有任何限制。将Excel文件转换为其他格式（例如PDF / XPS /图像）时，您只能获得PDF / XPS /图像的前3页。
